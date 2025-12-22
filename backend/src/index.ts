f031243c038d9969792e4a9b52f8af460977225e
Adrian Perez <appperez@ucdavis.edu>
2025-12-22 01:08:43 -0800

    Build and hook up frontend with backend

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const route = app.get('/api', (c) => {
+  return c.text('Hello Hono, coming from node.js!')
+})
+
+const server = serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+// graceful shutdown
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  server.close((err: any) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
+
+// For now we are only exporting one type of route
+// In the future, when we have multiple routes for and models
+// we are going to have to find a way to export multiple routes
+export type AppType = typeof route
+export default app
