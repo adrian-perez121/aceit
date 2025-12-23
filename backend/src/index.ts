1a2f19af246c3141f85e76db8d3dd971e04972da
Adrian Perez <appperez@ucdavis.edu>
2025-12-22 18:24:19 -0800

    Reformatting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-const route = app.get('/api', (c) => {
-  return c.text('Hello Hono, coming from node.js!')
-})
+const route = app.get("/api", (c) => {
+  return c.text("Hello Hono, coming from node.js!");
+});
 
-const server = serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const server = serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
 
 // graceful shutdown
-process.on('SIGINT', () => {
-  server.close()
-  process.exit(0)
-})
-process.on('SIGTERM', () => {
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
   server.close((err: any) => {
     if (err) {
-      console.error(err)
-      process.exit(1)
+      console.error(err);
+      process.exit(1);
     }
-    process.exit(0)
-  })
-})
+    process.exit(0);
+  });
+});
 
 // For now we are only exporting one type of route
 // In the future, when we have multiple routes for and models
 // we are going to have to find a way to export multiple routes
-export type AppType = typeof route
-export default app
+export type AppType = typeof route;
+export default app;
