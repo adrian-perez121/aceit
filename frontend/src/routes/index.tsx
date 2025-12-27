5d69d1becfdb50d071a8b358d9d8926c3a242bdf
Adrian Perez <appperez@ucdavis.edu>
2025-12-26 21:05:36 -0800

    Implement routing foundation

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+      <>
+        <div>This is the homepage</div>
+        <div>Hello "/"!</div>
+      </>
+  )
+}
